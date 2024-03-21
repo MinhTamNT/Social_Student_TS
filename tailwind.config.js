fad66924e64cdbc88f8fc838be80a147eb79de6c
MinhTamNT <minhtam78945@gmail.com>
2024-03-21 15:24:51 +0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora"],
+      },
+      fontSize: {
+        13: "13px",
+        14: "14px",
+        16: "16px",
+        20: "20px",
+        26: "26px",
+        24: "24px",
+        32: "32px",
+        36: "36px",
+        48: "48px",
+      },
+      boxShadow: {
+        md: "6px 6px 16px 0 rgba(0, 0, 0, 0.25),-4px -4px 12px 0 rgba(255, 255, 255, 0.3);",
+      },
+    },
+  },
+  plugins: [],
+};
